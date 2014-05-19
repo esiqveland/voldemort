e82828cdec2efd32344867d8a3b0c4d1eed6ee0c
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-05-19 12:25:12 +0200

    merge headmaster

--- src/java/voldemort/headmaster/Headmaster.java
+++ src/java/voldemort/headmaster/Headmaster.java
@@ -10,10 +10,6 @@ import org.slf4j.LoggerFactory;
 import voldemort.client.rebalance.RebalancePlan;
 import voldemort.cluster.Cluster;
 import voldemort.cluster.Node;
-import voldemort.headmaster.sigar.NodeStatus;
-import voldemort.headmaster.sigar.SigarReceiver;
-import voldemort.headmaster.sigar.SigarStatusMessage;
-import voldemort.headmaster.sigar.StatusMessageListener;
 import voldemort.server.VoldemortConfig;
 
 
